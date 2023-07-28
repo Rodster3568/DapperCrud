310b167d0ea6b9e5dbca84395142b81d0b9a51f3
Kopliness <mr.vladlis3568@gmail.com>
2023-07-28 22:42:50 +0300

    add http methods

--- DapperCrud/ContactBook.cs
+++ DapperCrud/ContactBook.cs
@@ -1,7 +1,12 @@
-﻿namespace DapperCrud
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DapperCrud
 {
     public class ContactBook
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Firstname { get; set; } = string.Empty;
--- DapperCrud/Controllers/ContactBookController.cs
+++ DapperCrud/Controllers/ContactBookController.cs
@@ -10,16 +10,50 @@ namespace DapperCrud.Controllers
     public class ContactBookController : ControllerBase
     {
         private readonly IConfiguration _configuration;
+        private readonly SqlConnection _connection;
+
         public ContactBookController(IConfiguration configuration)
         {
             _configuration = configuration;
+            _connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
         }
+
         [HttpGet]
         public async Task<ActionResult<List<ContactBook>>> GetAllContactBooks()
         {
-            using var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
-            var contacts = await connection.QueryAsync<ContactBook>("select * from ContactBooks");
+            IEnumerable<ContactBook> contacts = await SelectAllContacts();
             return Ok(contacts);
         }
-    } 
+
+        [HttpGet("{contactId}")]
+        public async Task<ActionResult<List<ContactBook>>> GetContact(int contactId)
+        {
+            var contact = await _connection.QueryFirstAsync<ContactBook>("select * from ContactBooks where id=@Id",
+                new { Id = contactId });
+            return Ok(contact);
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<ContactBook>>> CreateContact(ContactBook contact)
+        {
+            await _connection.ExecuteAsync("insert into ContactBooks (name, firstname, lastname, place) values (@Name, @FirstName, @LastName, @Place)", contact);
+            return Ok(await SelectAllContacts());
+        }
+        [HttpPut]
+        public async Task<ActionResult<List<ContactBook>>> UpdateContact(ContactBook contact)
+        {
+            await _connection.ExecuteAsync("update ContactBooks set name = @Name, " +
+                "firstname = @FirstName, lastname=@LastName, place=@Place where id=@Id", contact);
+            return Ok(await SelectAllContacts());
+        }
+        [HttpDelete("{contactId}")]
+        public async Task<ActionResult<List<ContactBook>>> DeleteContact(int contactId)
+        {
+            await _connection.ExecuteAsync("delete from ContactBooks where id= @Id", new {Id= contactId});
+            return Ok(await SelectAllContacts());
+        }
+        private async Task<IEnumerable<ContactBook>> SelectAllContacts()
+        {
+            return await _connection.QueryAsync<ContactBook>("select * from ContactBooks");
+        }
+    }
 }
