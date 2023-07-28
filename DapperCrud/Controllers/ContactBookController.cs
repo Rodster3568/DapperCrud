a92efa64e05092cbb442802e774a2ae7cc302d81
Kopliness <mr.vladlis3568@gmail.com>
2023-07-28 20:50:15 +0300

    added get method

--- DapperCrud/Controllers/ContactBookController.cs
+++ DapperCrud/Controllers/ContactBookController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 
 namespace DapperCrud.Controllers
 {
@@ -7,5 +9,17 @@ namespace DapperCrud.Controllers
     [ApiController]
     public class ContactBookController : ControllerBase
     {
-    }
+        private readonly IConfiguration _configuration;
+        public ContactBookController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<ContactBook>>> GetAllContactBooks()
+        {
+            using var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+            var contacts = await connection.QueryAsync<ContactBook>("select * from ContactBooks");
+            return Ok(contacts);
+        }
+    } 
 }
